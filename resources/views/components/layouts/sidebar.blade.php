d8d8fe6bc510e4dacb2da537b2fab0aa9610ee58
jundialfaruqi <jundialfaruqi@gmail.com>
2025-06-19 00:30:06 +0700

    migrasi jumbotron

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -7,21 +7,14 @@
         <h1 class="navbar-brand d-none-navbar-horizontal pt-lg-5">
             <a wire:navigate href="{{ route('dashboard.index') }}">
                 <span class="navbar-brand-image">
-                    {{-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-box">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5" />
-                        <path d="M12 12l8 -4.5" />
-                        <path d="M12 12l0 9" />
-                        <path d="M12 12l-8 -4.5" />
-                    </svg> --}}
                     <a href=".">
                         <img src="{{ asset('nav-brand.png') }}" width="30" alt="JWS Diskominfo"
                             class="navbar-brand-image">
+                        JWS Diskominfo
                     </a>
+
                 </span>
-                <span class="navbar-brand-text">JWS Diskominfo</span>
+                {{-- <span class="navbar-brand-text">JWS Diskominfo</span> --}}
             </a>
         </h1>
         <div class="navbar-nav flex-row d-lg-none">
@@ -140,6 +133,26 @@
                             </span>
                             <span class="nav-link-title">
                                 Profil Masjid
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item {{ request()->routeIs('profilmasjid.index') ? 'active' : '' }}">
+                        <a wire:navigate class="nav-link" href="{{ route('profilmasjid.index') }}">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="icon icon-tabler icons-tabler-outline icon-tabler-photo">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M15 8h.01" />
+                                    <path
+                                        d="M3 6a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12z" />
+                                    <path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l5 5" />
+                                    <path d="M14 14l1 -1c.928 -.893 2.072 -.893 3 0l3 3" />
+                                </svg>
+                            </span>
+                            <span class="nav-link-title">
+                                Jumbotron
                             </span>
                         </a>
                     </li>
