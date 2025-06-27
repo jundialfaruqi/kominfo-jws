b5402c86b2a1fb99c4fd03facc9dc7ca1c56adc1
jundialfaruqi <jundialfaruqi@gmail.com>
2025-06-28 03:50:34 +0700

    CSS BLUE SIMPLE

--- resources/views/livewire/firdaus/mosque-info.blade.php
+++ resources/views/livewire/firdaus/mosque-info.blade.php
@@ -7,15 +7,9 @@
                 <img src="{{ asset($profil->logo_pemerintah) }}" alt="Logo Pemerintah" class="logo logo-pemerintah">
             @endif
         </div>
-        {{-- @if ($profil->logo_pemerintah)
-            <img src="{{ asset($profil->logo_pemerintah) }}" alt="Logo Pemerintah" class="logopemerintah logo-pemerintah">
-        @endif --}}
         <div class="mosque-text">
             <h1><span class="mosque-name-highlight">{{ ucwords(strtolower($profil->name)) }}</span></h1>
             <p class="mosque-address">{{ ucwords(strtolower($profil->address)) }}</p>
         </div>
-        {{-- @if ($profil->logo_masjid)
-            <img src="{{ asset($profil->logo_masjid) }}" alt="Logo Masjid" class="logomasjid logo-masjid">
-        @endif --}}
     </div>
 </div>
