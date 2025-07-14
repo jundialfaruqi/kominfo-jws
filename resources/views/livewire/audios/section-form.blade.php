0777b1fc455965fcd44a319b38a11161a40cfa65
jundialfaruqi <jundialfaruqi@gmail.com>
2025-07-15 02:50:21 +0700

    validasi

--- resources/views/livewire/audios/section-form.blade.php
+++ resources/views/livewire/audios/section-form.blade.php
@@ -142,6 +142,33 @@
                                 <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                                 <span class="small">Loading...</span>
                             </div>
+                            @if ($audio1Uploaded)
+                                <div class="p-2 my-2 border border-success rounded-4">
+                                    <div class="text-center text-success">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-circle-dashed-check">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" />
+                                            <path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" />
+                                            <path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" />
+                                            <path d="M8.56 20.31a9 9 0 0 0 3.44 .69" />
+                                            <path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" />
+                                            <path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" />
+                                            <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
+                                            <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
+                                            <path d="M9 12l2 2l4 -4" />
+                                        </svg>
+                                    </div>
+                                    <div class="mt-2 text-center">
+                                        <small class="text-success">
+                                            Pemeriksaan file audio berhasil. File aman untuk
+                                            disimpan. Klik 'Simpan' untuk melanjutkan.
+                                        </small>
+                                    </div>
+                                </div>
+                            @endif
                             <div class="d-flex align-items-center gap-2">
                                 <input type="file"
                                     class="form-control my-2 rounded-4 @error('audio1') is-invalid @enderror"
@@ -251,6 +278,33 @@
                                     aria-hidden="true"></span>
                                 <span class="small">Loading...</span>
                             </div>
+                            @if ($audio2Uploaded)
+                                <div class="p-2 my-2 border border-success rounded-4">
+                                    <div class="text-center text-success">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-circle-dashed-check">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" />
+                                            <path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" />
+                                            <path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" />
+                                            <path d="M8.56 20.31a9 9 0 0 0 3.44 .69" />
+                                            <path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" />
+                                            <path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" />
+                                            <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
+                                            <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
+                                            <path d="M9 12l2 2l4 -4" />
+                                        </svg>
+                                    </div>
+                                    <div class="mt-2 text-center">
+                                        <small class="text-success">
+                                            Pemeriksaan file audio berhasil. File aman untuk
+                                            disimpan. Klik 'Simpan' untuk melanjutkan.
+                                        </small>
+                                    </div>
+                                </div>
+                            @endif
                             <div class="d-flex align-items-center gap-2">
                                 <input type="file"
                                     class="form-control my-2 rounded-4 @error('audio2') is-invalid @enderror"
@@ -362,6 +416,33 @@
                                     aria-hidden="true"></span>
                                 <span class="small">Loading...</span>
                             </div>
+                            @if ($audio3Uploaded)
+                                <div class="p-2 my-2 border border-success rounded-4">
+                                    <div class="text-center text-success">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-circle-dashed-check">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" />
+                                            <path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" />
+                                            <path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" />
+                                            <path d="M8.56 20.31a9 9 0 0 0 3.44 .69" />
+                                            <path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" />
+                                            <path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" />
+                                            <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
+                                            <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
+                                            <path d="M9 12l2 2l4 -4" />
+                                        </svg>
+                                    </div>
+                                    <div class="mt-2 text-center">
+                                        <small class="text-success">
+                                            Pemeriksaan file audio berhasil. File aman untuk
+                                            disimpan. Klik 'Simpan' untuk melanjutkan.
+                                        </small>
+                                    </div>
+                                </div>
+                            @endif
                             <div class="d-flex align-items-center gap-2">
                                 <input type="file"
                                     class="form-control my-2 rounded-4 @error('audio3') is-invalid @enderror"
