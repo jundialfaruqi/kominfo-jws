da6ab5c949895f5aaed48be4136237d1048af14c
Ahmad Mazlan <43345584+xmazlan@users.noreply.github.com>
2025-10-18 16:46:18 +0700

    fix seeder priority for first time install

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,41 +15,29 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(50)->create();
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'role' => 'Admin',
+            'phone' => '081234567890',
+            'password' => Hash::make('admin123'),
+            'address' => 'Pekanbaru',
+            'status' => 'Active',
+        ]);
 
+        User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@mail.com',
+            'role' => 'Super Admin',
+            'phone' => '081234567890',
+            'password' => Hash::make('admin123'),
+            'address' => 'Pekanbaru',
+            'status' => 'Active',
+        ]);
 
-
-        // User::factory()->create([
-        //     'name' => 'Admin',
-        //     'email' => 'admin@mail.com',
-        //     'role' => 'Admin',
-        //     'phone' => '081234567890',
-        //     'password' => Hash::make('admin123'),
-        //     'address' => 'Pekanbaru',
-        //     'status' => 'Active',
-        // ]);
-
-        // User::factory()->create([
-        //     'name' => 'Super Admin',
-        //     'email' => 'superadmin@mail.com',
-        //     'role' => 'Super Admin',
-        //     'phone' => '081234567890',
-        //     'password' => Hash::make('admin123'),
-        //     'address' => 'Pekanbaru',
-        //     'status' => 'Active',
-        // ]);
-
-        // $this->call([
-        //     ProfilSeeder::class,
-        //     AdzanSeeder::class,
-        //     MarqueeSeeder::class,
-        // ]);
-
-        // User::factory()->create([
-        //     'name' => 'User',
-        //     'email' => 'user@mail.com',
-        //     'role' => 'User',
-        //     'password' => Hash::make('admin123'),
-        //     'address' => 'Pekanbaru',
-        // ]);
+        $this->call([
+            RolePermissionSeeder::class,
+            AssignRolesToUsersSeeder::class,
+        ]);
     }
 }
