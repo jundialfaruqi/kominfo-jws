00aa1cb1c9b0adaa30b71522e99a303845911c07
jundialfaruqi <jundialfaruqi@gmail.com>
2025-10-30 11:56:43 +0700

    add slug url to user table

--- resources/views/livewire/admin/user/table.blade.php
+++ resources/views/livewire/admin/user/table.blade.php
@@ -35,6 +35,27 @@
                     </td>
                     <td>
                         {{ $users->profil->name ?? '-' }}
+                        @php $slug = $users->profil?->slug; @endphp
+
+                        @if ($slug)
+                            @php $url = route('firdaus', $slug); @endphp
+                            <a href="{{ $url }}" target="_blank"
+                                class="d-flex align-items-center gap-2 text-blue-600 hover:underline">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="icon icon-tabler icons-tabler-outline icon-tabler-link">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M9 15l6 -6" />
+                                    <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" />
+                                    <path
+                                        d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
+                                </svg>
+                                {{ $url }}
+                            </a>
+                        @else
+                            <span class="text-gray-400"></span>
+                        @endif
                     </td>
                     <td>{{ $users->phone }}</td>
                     @if ($users->role == 'Super Admin')
@@ -157,8 +178,9 @@
                         @endphp
 
                         @if ($canEdit)
-                            <button wire:click="edit('{{ $users->id }}')" class="btn py-2 px-2 rounded-3 shadow-sm"
-                                data-bs-toggle="modal" data-bs-target="#editModal">
+                            <button wire:click="edit('{{ $users->id }}')"
+                                class="btn py-2 px-2 rounded-3 shadow-sm" data-bs-toggle="modal"
+                                data-bs-target="#editModal">
                                 <span wire:loading.remove wire:target="edit('{{ $users->id }}')">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
--- resources/views/livewire/profil/section-table.blade.php
+++ resources/views/livewire/profil/section-table.blade.php
@@ -45,7 +45,18 @@
                                 {{ $loop->iteration + ($profilList->currentPage() - 1) * $profilList->perPage() }}</td>
                             <td class="text-wrap">
                                 <div>{{ $profil->name }}</div>
-                                <div class="text-muted">{{ $profil->user->name ?? '-' }}</div>
+                                <div class="text-muted">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round"
+                                        class="icon icon-tabler icons-tabler-outline icon-tabler-mail">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path
+                                            d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+                                        <path d="M3 7l9 6l9 -6" />
+                                    </svg>
+                                    {{ $profil->user->email ?? '-' }}
+                                </div>
                             </td>
                             <td class="text-wrap">{{ $profil->address }}</td>
                             <td class="text-wrap">{{ $profil->phone }}</td>
