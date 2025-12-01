683abb687a217be26fbacaaaf23796d016fefb4d
jundialfaruqi <jundialfaruqi@gmail.com>
2025-12-01 15:11:05 +0700

    fix(AgendaMasjid): change agenda sorting from desc to asc

--- app/Livewire/Agenda/AgendaMasjid.php
+++ app/Livewire/Agenda/AgendaMasjid.php
@@ -66,7 +66,7 @@ class AgendaMasjid extends Component
                     ;
                 });
             })
-            ->orderBy('date', 'desc')
+            ->orderBy('date', 'asc')
             ->orderBy('id', 'asc')
             ->with(['profilMasjid'])
             ->paginate($this->paginate);
